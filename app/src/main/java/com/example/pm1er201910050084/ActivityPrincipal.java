a2522df67abbc30615358443899ff58a445b36fe
Wilmer Bordas <108447599+WBordas56@users.noreply.github.com>
2022-08-25 22:35:58 -0600

    Initial Commit

---
+++ app/src/main/java/com/example/pm1er201910050084/ActivityPrincipal.java
@@ -0,0 +1,14 @@
+package com.example.pm1er201910050084;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+}
