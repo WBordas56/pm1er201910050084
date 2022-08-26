171754dd375299d2815768d7bd49850dbc9815b2
Wilmer Bordas <108447599+WBordas56@users.noreply.github.com>
2022-08-26 00:05:05 -0600

    Initial Commit

--- app/src/main/java/com/example/pm1er201910050084/ActivityPrincipal.java
+++ app/src/main/java/com/example/pm1er201910050084/ActivityPrincipal.java
@@ -2,13 +2,21 @@ package com.example.pm1er201910050084;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaRecorder; //Importamos la clase para grabar video
 import android.os.Bundle;
+import android.widget.Button;
 
 public class ActivityPrincipal extends AppCompatActivity {
+
+    private MediaRecorder grabacion;
+    private String archivoSalida = null;
+    private Button btn_grabar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        btn_grabar = (Button) findViewById(R.id.btn_rec);
     }
 }
