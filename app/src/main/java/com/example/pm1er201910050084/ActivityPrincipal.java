2aa6ad7874157fad6fdd4bac9bbfa1081a9d27f8
Wilmer Bordas <108447599+WBordas56@users.noreply.github.com>
2022-08-26 01:15:37 -0600

    Initial Commit

--- app/src/main/java/com/example/pm1er201910050084/ActivityPrincipal.java
+++ app/src/main/java/com/example/pm1er201910050084/ActivityPrincipal.java
@@ -1,10 +1,22 @@
 package com.example.pm1er201910050084;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
 import android.media.MediaRecorder; //Importamos la clase para grabar video
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class ActivityPrincipal extends AppCompatActivity {
 
@@ -18,5 +30,51 @@ public class ActivityPrincipal extends AppCompatActivity {
         setContentView(R.layout.activity_principal);
 
         btn_grabar = (Button) findViewById(R.id.btn_rec);
+
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(ActivityPrincipal.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO}, 1000);
+        }
     }
+
+
+    public void Grabar(View view){
+        if(grabacion == null){
+            archivoSalida = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Grabacion.mp3";
+            grabacion = new MediaRecorder();
+            grabacion.setAudioSource(MediaRecorder.AudioSource.MIC);
+            grabacion.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            //grabacion.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+            grabacion.setOutputFile(archivoSalida);
+
+            try{
+                grabacion.prepare();
+                grabacion.start();
+            }catch (Exception e){
+
+            }
+
+            Toast.makeText(this, "Grabando", Toast.LENGTH_SHORT).show();
+        } else if(grabacion != null){
+            grabacion.stop();
+            grabacion.release();
+            grabacion = null;
+            Toast.makeText(this, "Grabacion Finalizada", Toast.LENGTH_SHORT).show();
+        }
+    } //fin de funcion grabar()
+
+    public void Reproducir(View view) {
+        MediaPlayer mediaPlayer = new MediaPlayer();
+        try {
+            mediaPlayer.setDataSource(archivoSalida);
+            mediaPlayer.prepare();
+        }catch (Exception e){
+
+        }
+
+        mediaPlayer.start();
+        Toast.makeText(this, "Reproduciendo audio", Toast.LENGTH_SHORT).show();
+
+    } //fin de funcion reproducir()
+
+
 }
--- app/src/main/java/com/example/pm1er201910050084/MainActivity.java
+++ app/src/main/java/com/example/pm1er201910050084/MainActivity.java
@@ -8,10 +8,12 @@ import android.os.Handler;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     ImageView imageLogo;
-    Animation animation1;
+    TextView texto;
+    Animation animation1,animation2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,12 +21,15 @@ public class MainActivity extends AppCompatActivity {
         init();
 
         imageLogo.setAnimation(animation1);
+        texto.setAnimation(animation2);
         new Handler().postDelayed(this::run, 3000);
     }
 
     private void init(){
         imageLogo = findViewById(R.id.imageViewLogo);
+        texto = findViewById(R.id.txtvU);
         animation1 = AnimationUtils.loadAnimation(this, R.anim.scroll_up);
+        animation2 = AnimationUtils.loadAnimation(this, R.anim.scroll_down);
     }
 
     private void run() {
