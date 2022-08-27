c15a826e2f7988b3bec4d2f423676be58edc07c3
Wilmer Bordas <108447599+WBordas56@users.noreply.github.com>
2022-08-27 01:11:49 -0600

    Initial commit

--- app/src/main/java/com/example/pm1er201910050084/ActivityPrincipal.java
+++ app/src/main/java/com/example/pm1er201910050084/ActivityPrincipal.java
@@ -1,80 +1,249 @@
 package com.example.pm1er201910050084;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.media.MediaPlayer;
-import android.media.MediaRecorder; //Importamos la clase para grabar video
+import android.graphics.Bitmap;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
+import android.provider.Settings;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 
 public class ActivityPrincipal extends AppCompatActivity {
 
-    private MediaRecorder grabacion;
-    private String archivoSalida = null;
-    private Button btn_grabar;
+    Button btnguardar, btnlistado,btntomarft,btnseleccionarft;
+    EditText txtlatitud,txtlongitud,txtdescripcion,txttelefono;
+    ImageView ivfoto,btngrabar,btnreproducir;
+
+
+    Bitmap imagen;
+
+    static final int RESULT_GALLERY_IMG = 200;
+    static final int PETICION_ACCESO_CAM = 100;
+    static final int TAKE_PIC_REQUEST = 101;
+
+    public static String latitud = "";
+    public static String longitud = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
 
-        btn_grabar = (Button) findViewById(R.id.btn_rec);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    //Manifest.permission.ACCESS_COARSE_LOCATION
+            }, 1000);
+        } else {
 
-        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(ActivityPrincipal.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO}, 1000);
+        }
+
+        ivfoto = (ImageView) findViewById(R.id.Foto);
+        btntomarft = (Button) findViewById(R.id.btn_tomar_foto);
+        btnseleccionarft = (Button) findViewById(R.id.btn_seleccionar_foto);
+        btngrabar = (ImageView) findViewById(R.id.btn_grabar);
+        btnreproducir = (ImageView) findViewById(R.id.btn_reproducir);
+        txtlatitud = (EditText) findViewById(R.id.txt_latitud);
+        txtlongitud = (EditText) findViewById(R.id.txt_longitud);
+        txtdescripcion = (EditText) findViewById(R.id.txt_descripcion);
+        txttelefono = (EditText) findViewById(R.id.txt_telefono);
+        btnguardar = (Button) findViewById(R.id.btn_guardar);
+        btnlistado = (Button) findViewById(R.id.btn_lista);
+
+        btntomarft.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                permisos();
+            }
+        });
+
+        btnseleccionarft.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GaleriaImagenes();
+            }
+        });
+
+        //valida si tiene los permisos de ser asi manda a llamar el metodo locationStart()
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 1000);
+        } else {
+            locationStart();
+        }
+
+    } // fin OnCreate
+
+    //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    // Permisos
+
+    private void permisos() {
+
+        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CAMERA},PETICION_ACCESO_CAM);
+        }else{
+            tomarFoto();
         }
     }
 
+    //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    // Obtener Foto
 
-    public void Grabar(View view){
-        if(grabacion == null){
-            archivoSalida = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Grabacion.mp3";
-            grabacion = new MediaRecorder();
-            grabacion.setAudioSource(MediaRecorder.AudioSource.MIC);
-            grabacion.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-            //grabacion.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
-            grabacion.setOutputFile(archivoSalida);
+    private void tomarFoto() {
+        Intent takepic = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
-            try{
-                grabacion.prepare();
-                grabacion.start();
-            }catch (Exception e){
-
-            }
-
-            Toast.makeText(this, "Grabando", Toast.LENGTH_SHORT).show();
-        } else if(grabacion != null){
-            grabacion.stop();
-            grabacion.release();
-            grabacion = null;
-            Toast.makeText(this, "Grabacion Finalizada", Toast.LENGTH_SHORT).show();
+        if(takepic.resolveActivity(getPackageManager()) != null)
+        {
+            startActivityForResult(takepic,TAKE_PIC_REQUEST);
         }
-    } //fin de funcion grabar()
+    }
 
-    public void Reproducir(View view) {
-        MediaPlayer mediaPlayer = new MediaPlayer();
+    private void GaleriaImagenes() {
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(photoPickerIntent, RESULT_GALLERY_IMG);
+    }
+
+    private String GetStringImage(Bitmap photo) {
+
         try {
-            mediaPlayer.setDataSource(archivoSalida);
-            mediaPlayer.prepare();
-        }catch (Exception e){
+            ByteArrayOutputStream ba = new ByteArrayOutputStream();
+            photo.compress(Bitmap.CompressFormat.JPEG, 70, ba);
+            byte[] imagebyte = ba.toByteArray();
+            String encode = Base64.encodeToString(imagebyte, Base64.DEFAULT);
 
+            return encode;
+        }catch (Exception ex)
+        {
+            ex.toString();
+        }
+        return "";
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Uri imageUri;
+        //obtener la imagen por el almacenamiento interno
+        if(resultCode==RESULT_OK && requestCode==RESULT_GALLERY_IMG)
+        {
+
+            imageUri = data.getData();
+            ivfoto.setImageURI(imageUri);
+            try {
+                imagen=MediaStore.Images.Media.getBitmap(this.getContentResolver(),imageUri);
+
+            }catch (Exception e)
+            {
+                Toast.makeText(getApplicationContext(),"Error al seleccionar imagen", Toast.LENGTH_SHORT).show();
+            }
+        }
+        //obtener la imagen por la camara
+        if(requestCode == TAKE_PIC_REQUEST && resultCode == RESULT_OK)
+        {
+            Bundle extras = data.getExtras();
+            imagen = (Bitmap) extras.get("data");
+            ivfoto.setImageBitmap(imagen);
         }
 
-        mediaPlayer.start();
-        Toast.makeText(this, "Reproduciendo audio", Toast.LENGTH_SHORT).show();
 
-    } //fin de funcion reproducir()
+    }
+
+
+    //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    // Obtener Ubicacion
+
+    private void locationStart() {
+        LocationManager mlocManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Localizacion Local = new Localizacion();
+        Local.setMainActivity(this);
+        final boolean gpsEnabled = mlocManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        if (!gpsEnabled) {
+            //SE VA A LA CONFIGURACION DEL SISTEMA PARA QUE ACTIVE EL GPS UNA VEZ QUE INICIA LA APLICACION
+            Intent settingsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(settingsIntent);
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 1000);
+        }
+        mlocManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, (LocationListener) Local);
+        mlocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, (LocationListener) Local);
+
+    }
+
+    public class Localizacion implements LocationListener{
+
+        ActivityPrincipal mainActivity;
+
+        public void setMainActivity(ActivityPrincipal mainActivity) {
+            this.mainActivity = mainActivity;
+        }
+
+        @Override
+        public void onLocationChanged(@NonNull Location location) {
+            location.getLatitude();
+            location.getLongitude();
+
+            String Text = "Mi ubicacion actual es: " + "\n Lat = "
+                    + location.getLatitude() + "\n Long = " + location.getLongitude();
+
+
+            ActivityPrincipal.setLatitud(location.getLatitude()+"");
+            ActivityPrincipal.setLongitud(location.getLongitude()+"");
+            txtlatitud.setText(location.getLatitude()+"");
+            txtlongitud.setText(location.getLongitude()+"");
+            this.mainActivity.setLocation(location);
+        }
+    }
+
+    public void setLocation(Location loc) {
+        //Obtener la direccion de la calle a partir de la latitud y la longitud
+        if (loc.getLatitude() != 0.0 && loc.getLongitude() != 0.0) {
+            try {
+                Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+                List<Address> list = geocoder.getFromLocation(
+                        loc.getLatitude(), loc.getLongitude(), 1);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    ///
+
+    public static void setLatitud(String latitud) {
+        ActivityPrincipal.latitud = latitud;
+    }
+
+    public static void setLongitud(String longitud) {
+        ActivityPrincipal.longitud = longitud;
+    }
+
+
 
 
 }
